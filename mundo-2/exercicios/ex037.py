5069438daa3529a4110c34412d4947df36b6b472
Robson Ferreira <robsondonasferreira@hotmail.com>
2026-01-09 07:25:54 -0300

    Fix: Sync project state and update exercises

--- mundo-2/exercicios/ex037.py
+++ mundo-2/exercicios/ex037.py
@@ -4,10 +4,10 @@ numero = int(input('Digite um numero: '))
 print('Escolha uma das bases para conversão: ')
 opcao = int(input('Digite a opção desejada 1, 2 ou 3: '))
 if opcao == 1:
-    print(f'O número {numero} será convertido para binario e o resultado é {bin(numero)}')
+    print(f'O número {numero} será convertido para binario e o resultado é {bin(numero)[2:]}')
 elif opcao == 2:
-    print(f'O número {numero} será convertido para octal e o resultado é {oct(numero)}')
+    print(f'O número {numero} será convertido para octal e o resultado é {oct(numero)[2:]}')
 elif opcao == 3:
-    print(f'O número {numero} será convertido para hexadecimal e o resultado é {hex(numero)}')
+    print(f'O número {numero} será convertido para hexadecimal e o resultado é {hex(numero)[2:]}')
 else:
     print('Opção invalida! Tente novamente.')
