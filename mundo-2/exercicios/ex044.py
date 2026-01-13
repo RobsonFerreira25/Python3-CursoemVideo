1f762e21fc6f764e9e11aa2a2403bde05c488260
Robson Ferreira <robsondonasferreira@hotmail.com>
2026-01-13 07:28:05 -0300

    Finaliza implementação do exercício 044: Gerenciador de Pagamentos

--- mundo-2/exercicios/ex044.py
+++ mundo-2/exercicios/ex044.py
@@ -1,3 +1,4 @@
+'''
 # Sistema de pagamento.
 # Coleta de dados.
 preco_normal_produto = float(input('Digite o preço normal do produto: '))
@@ -18,3 +19,33 @@ elif condicao_pagamento == 4:
     print(f'Forma de pagamento: cartão parcelado em 3x ou mais, valor final do produto: R${preco_normal_produto:.2f}')
 else:
     print('Opção invalida')
+'''
+
+# Forma aprimorada, Curso em Video
+print('{:=^40}'.format(' LOJAS KN '))
+compras = float(input('Digite o valor das compras: R$'))
+print('''
+FORMAS DE PAGAMENTO:
+[1] À vista dinheiro
+[2] À vista cartão 5% de desconto
+[3] 2x no cartão sem juros
+[4] 3x ou mais no cartão 20% de juros
+''')
+opcao = int(input('Digite a opção de pagamento: '))
+if opcao == 1:
+    compras = compras - (compras * 0.1)
+    print(f'Pagamento em dinheiro com 10% de desconto, valor final: R${compras:.2f}')
+elif opcao == 2:
+    compras = compras - (compras * 0.05)
+    print(f'Pagamento em cartão a vista com 5% de desconto, valor final: R${compras:.2f}')
+elif opcao == 3:
+    vparcelas = compras / 2
+    print(f'Pagamento em cartão parcelado em 2x SEM JUROS, valor final: R${compras:.2f}')
+    print(f'Você pagará R${compras:.2f} em 2 parcelas de R${vparcelas:.2f}')
+elif opcao == 4:
+    print(f'Pagamento em cartão parcelado em 3x ou mais COM JUROS de 20%')
+    nparcelas = int(input('Digite o número de parcelas: '))
+    valor_final = compras + (compras * 0.2)
+    vparcelas = valor_final / nparcelas
+    print(f'Pagamento em cartão parcelado em {nparcelas}x COM JUROS de 20%, valor final: R${valor_final:.2f}')
+    print(f'Você pagará R${valor_final:.2f} em {nparcelas}x de R${vparcelas:.2f}')
