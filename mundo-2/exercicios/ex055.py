7d7e66151215db5ef283283216e9213080fa0f79
Robson Ferreira <robsondonasferreira@hotmail.com>
2026-01-16 13:23:40 -0300

    Create ex055.py

---
+++ mundo-2/exercicios/ex055.py
@@ -0,0 +1,9 @@
+# ler o peso de 5 pessoas e mostrar o maior e o menor peso
+from os import system
+system('cls')
+peso_maior = 0
+peso_menor = 0
+
+for c in range(5):
+    peso = float(input('Digite o seu peso: '))
+print(c)
