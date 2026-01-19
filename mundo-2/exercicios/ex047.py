5bd1d89d840f3177c3c4f0a8b7ebc67430396394
Robson Ferreira <robsondonasferreira@hotmail.com>
2026-01-19 07:38:48 -0300

    Update ex047.py

--- mundo-2/exercicios/ex047.py
+++ mundo-2/exercicios/ex047.py
@@ -3,7 +3,6 @@ from os import system
 
 system('cls')
 print('{:=^40}'.format(' Contagem de numeros pares entre 1 e 50 '))
-for c in range(0, 51, 2): # Contagem de 0 até 50 com passo de 2
-    print(c) # Imprime os numeros pares
+for c in range(2, 51, 2): # Contagem de 2 até 50 com passo de 2
+    print(c, end=' ') # Imprime os numeros pares
 
-system('pause')
