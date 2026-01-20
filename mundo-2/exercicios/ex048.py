27a5630796efe3a3291fc120e94ad7ccface2be4
Robson Ferreira <robsondonasferreira@hotmail.com>
2026-01-20 06:00:40 -0300

    Update ex048.py

--- mundo-2/exercicios/ex048.py
+++ mundo-2/exercicios/ex048.py
@@ -5,6 +5,5 @@ soma = 0
 for c in range(1, 501, 2):
     if c % 3 == 0: # Verifica se o número é divisível por 3
         soma += c # Soma os números divisiveis por 3
-        print(c)
 print(f'A soma de todos os números divisiveis por 3 entre 1 e 500 é: {soma}')
 system('pause')
