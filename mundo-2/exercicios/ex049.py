7455f82011daecb539bcdc1850890a7852f26ed0
Robson Ferreira <robsondonasferreira@hotmail.com>
2026-01-20 06:09:49 -0300

    Update ex049.py

--- mundo-2/exercicios/ex049.py
+++ mundo-2/exercicios/ex049.py
@@ -1,9 +1,16 @@
 # Tabuada
 from os import system
 system('cls')
+"""
 n1 = int(input('Digite um numero: '))
 for i in range(11):
     r = n1 * i
     print(f'{n1} X {i} = {r}')
 print(f'Essa é a tabuada de {n1}')
+"""
+
+# Resolução curso em video
+num = int(input('Digite um numero: '))
+for c in range (1, 11):
+    print(f'{num} X {c:2} = {num * c}')
 system('pause')
