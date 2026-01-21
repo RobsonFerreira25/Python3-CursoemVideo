8446ec2aedcdaff88dd3ca413ad941ac9879ab23
Robson Ferreira <robsondonasferreira@hotmail.com>
2026-01-21 06:56:05 -0300

    Update ex053.py

--- mundo-2/exercicios/ex053.py
+++ mundo-2/exercicios/ex053.py
@@ -1,6 +1,7 @@
 # Ler uma frase e dizer se ela é um palíndromo
 from os import system
 system('cls')
+"""
 frase = str(input('Digite uma frase: ')).strip().upper()
 frase = frase.replace(' ', '')
 frase = frase.split()
@@ -10,4 +11,15 @@ if frase == frase[::-1]:
     print('É um palíndromo!')
 else:
     print('Não é um palíndromo!')
+"""
+frase = str(input('Digite uma frase: ')).strip().upper()
+frase = frase.replace(' ', '')
+frase = frase.split()
+frase = ''.join(frase)
+for c in range(len(frase)):
+    if frase[c] != frase[-c - 1]:
+        print('Não é um palíndromo!')
+        break
+else:
+    print('É um palíndromo!')
 system('pause')
