6be80da78dac6a15f30266938736a744f0c9f715
Robson Ferreira <robsondonasferreira@hotmail.com>
2026-01-28 06:20:13 -0300

    Create ex060.py

---
+++ mundo-2/exercicios/ex060.py
@@ -0,0 +1,18 @@
+# fatorial de um numero em for 
+from os import system
+system('cls')
+"""
+num = int(input('Digite um número: '))
+fatorial = 1
+for i in range(1, num + 1):
+    fatorial *= i
+print(fatorial)
+"""
+
+# fatorial de um numero em while
+num = int(input('Digite um número: '))
+fatorial = 1
+while num != 0:
+    fatorial *= num
+    num -= 1
+print(fatorial)
